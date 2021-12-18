7503ad6e40bebf1790771160b0b2e5868b56471b
HannaParch <hanna.parchomenko@gmail.com>
2021-12-18 14:46:12 +0000

    Update to v3.0

--- testy.py
+++ testy.py
@@ -1,3 +1,6 @@
+
+
+
 sex = input('[k]obieta czy [m]ezczyzna ?:')
 if sex == 'k':
     print('mozesz uzywac aplikacje Droga Pani')
@@ -6,12 +9,23 @@ if sex == 'k':
     if wiek.isdigit() == False:
         exit("Wiek musi być liczbą")
     wiek = int(wiek)
-    if wiek >= 21 and wiek <= 65:
-        print("Witamy w aplikacji i smacznej degustacji!")
-    elif wiek > 65:
-        print("Jesteś już za stary/a na alkohol")
+    country = input('Where are you from? USA or EU')
+    if country == 'USA':
+     if wiek >= 21 and wiek <= 65:
+         print("Witamy w aplikacji i smacznej degustacji!")
+     elif wiek > 65:
+         print("Jesteś już za stary/a na alkohol")
+     else:
+         exit('jesteś za młody/a!, w USA wymagamy 21 lat')
+    if country == 'EU':
+         if wiek >= 18 and wiek <= 65:
+             print("Witamy w aplikacji i smacznej degustacji!")
+         elif wiek > 65:
+             print("Jesteś już za stary/a na alkohol")
+         else:
+             exit('jesteś za młody/a!, musisz miec 18 lat in EU')
     else:
-        exit('jesteś za młody/a!, w USA wymagamy 21 lat')
+        print('Kraj nieopisany')
 elif sex =='m':
     print('Ta aplikacja nie jest dla facetow, sorry!')
 
